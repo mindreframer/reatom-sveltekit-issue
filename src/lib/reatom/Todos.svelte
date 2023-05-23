8c708de12020e40b776d91d6165920326df8127c
Roman Heinrich <roman.heinrich@gmail.com>
2023-05-24 00:49:58 +0200

    Some testing

---
+++ src/lib/reatom/Todos.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { connectLogger, atom } from '@reatom/framework';
+	import { withSvelte, setupCtx } from '@reatom/npm-svelte';
+	import { ctx } from './todos';
+	connectLogger(ctx);
+	setupCtx(ctx);
+
+	const count = atom(0).pipe(withSvelte);
+</script>
+
+<button on:click={() => $count++}>
+	Clicked {$count}
+	{$count === 1 ? 'time' : 'times'}
+</button>
--- src/lib/reatom/todos.ts
+++ src/lib/reatom/todos.ts
@@ -8,17 +8,12 @@ interface Todo {
 // global application context
 export const ctx = createCtx();
 export const todosAtom = reatomArray<Todo>();
-
-ctx.subscribe(todosAtom, (todos) => {});
-
 export const addTodo = action((ctx, title: string) => {
 	todosAtom(ctx, (state) => {
 		state.push({ title: title, done: false });
 		return state;
 	});
 });
-
-// export const todosSortedAtom = todosAtom.toSorted(ctx, (a, b) => (a.title > b.title ? -1 : 1));
 
 export const myCountAtom = atom(0);
 export const increment = action((ctx, value: number) => {
