cb35576125a6516a654f8ed00406d490fc782ccc
Roman Heinrich <roman.heinrich@gmail.com>
2023-05-24 01:14:51 +0200

    Chore: formatting

--- src/lib/reatom/todos.ts
+++ src/lib/reatom/todos.ts
@@ -1,21 +1,21 @@
-import { reatomArray, createCtx, action, atom } from '@reatom/framework';
+import { action, atom, createCtx, reatomArray } from '@reatom/framework';
 
 interface Todo {
-	title: string;
-	done: boolean;
+  title: string;
+  done: boolean;
 }
 
 // global application context
 export const ctx = createCtx();
 export const todosAtom = reatomArray<Todo>();
 export const addTodo = action((ctx, title: string) => {
-	todosAtom(ctx, (state) => {
-		state.push({ title: title, done: false });
-		return state;
-	});
+  todosAtom(ctx, (state) => {
+    state.push({ title: title, done: false });
+    return state;
+  });
 });
 
 export const myCountAtom = atom(0);
 export const increment = action((ctx, value: number) => {
-	myCountAtom(ctx, (state) => state + value);
+  myCountAtom(ctx, (state) => state + value);
 }, 'increment');
